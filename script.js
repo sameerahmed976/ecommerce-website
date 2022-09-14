dceff6c85628cb75a57038af0ecdbd628b6af087
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-09-14 20:43:03 +0530

    added the cart toggle

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import { getElement } from "./src/getElement.js";
 const hamburger = getElement(".hamburger");
 const closeButton = getElement(".close");
 const sideNavbar = getElement(".side-navbar");
+const cartContainer = getElement(".cart");
+const cartOverlay = getElement(".cart-overlay ");
+const cartClose = getElement(".cart-close ");
 
 hamburger.addEventListener("click", () => {
   //   console.log("click");
@@ -11,4 +14,12 @@ hamburger.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   //   console.log("click");
   sideNavbar.classList.remove("show-side-bar");
+});
+cartContainer.addEventListener("click", () => {
+  //   console.log("click");
+  cartOverlay.classList.add("show-cart-overlay");
+});
+cartClose.addEventListener("click", () => {
+  //   console.log("click");
+  cartOverlay.classList.remove("show-cart-overlay");
 });
